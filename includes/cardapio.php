4a2fba14990c588f6ffd4833d99eff4bb4450e7b
guilherme-bezerra-castro <guilhermebezerradecastro@gmail.com>
2026-07-20 15:45:11 -0300

    refactor(head): change page title

--- includes/cardapio.php
+++ includes/cardapio.php
@@ -17,7 +17,7 @@ $secoes = $dadosCardapio['secoes'];
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Cardápio - Gostinho Natural</title>
+  <title>Gostinho Natural - Cardápio</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
   <link rel="stylesheet" href="../assets/css/home.css">
   <link rel="stylesheet" href="../assets/css/cardapio.css">
