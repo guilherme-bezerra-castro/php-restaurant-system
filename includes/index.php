491b774423ec37d3215d654b775e33ccc0246aa4
guilherme-bezerra-castro <guilhermebezerradecastro@gmail.com>
2026-08-28 21:27:23 -0300

    feat: turn error handling on in all public pages

--- includes/index.php
+++ includes/index.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/bootstrap.php';
 require_once __DIR__ . '/helpers.php';
 
 $conteudo       = require __DIR__ . '/data/conteudo_home.php';
--- includes/locais.php
+++ includes/locais.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once __DIR__ . '/bootstrap.php';
 require_once __DIR__ . '/helpers.php';
 
 $conteudo = require __DIR__ . '/data/conteudo_locais.php';
